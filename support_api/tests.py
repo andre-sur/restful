cf71a1202225668d08b25e6167896220c9b04b50
andre <andresuruguepro@gmail.com>
2025-07-22 12:18:02 +0200

    tests et record resultat on txt file

--- support_api/tests.py
+++ support_api/tests.py
@@ -2,13 +2,14 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from support_api.models import Project, Contributor, Issue, Comment
-from rest_framework.reverse import reverse
 import os
+import django
+
+# Configuration des settings Django si nécessaire
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest.settings')
+django.setup()
 
 User = get_user_model()
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'support_api.settings')
-
 
 class CommentPermissionsTest(TestCase):
     def setUp(self):
@@ -17,18 +18,25 @@ class CommentPermissionsTest(TestCase):
         self.contributor = User.objects.create_user(username='contrib', password='pass')
         self.stranger = User.objects.create_user(username='intrus', password='pass')
 
-        # Création du projet et assignation des contributeurs
-        self.project = Project.objects.create(title='Projet Test', description='desc', author=self.author)
+        # Création d'un projet avec contributeurs
+        self.project = Project.objects.create(title='Projet Test', description='desc', type='BACK_END', author=self.author)
         Contributor.objects.create(user=self.author, project=self.project)
         Contributor.objects.create(user=self.contributor, project=self.project)
 
         # Création d'une issue liée au projet
-        self.issue = Issue.objects.create(title='Issue test', description='desc', project=self.project, author=self.author)
+        self.issue = Issue.objects.create(
+            title='Issue test',
+            description='desc',
+            tag='BUG',
+            priority='HIGH',
+            project=self.project,
+            author=self.author
+        )
 
-        # Création d'un commentaire sur l'issue
+        # Création d'un commentaire lié à l'issue
         self.comment = Comment.objects.create(description='Un commentaire', author=self.author, issue=self.issue)
 
-        # Clients API pour chaque utilisateur
+        # Clients authentifiés
         self.client_author = APIClient()
         self.client_author.force_authenticate(user=self.author)
 
@@ -39,24 +47,29 @@ class CommentPermissionsTest(TestCase):
         self.client_stranger.force_authenticate(user=self.stranger)
 
     def test_stranger_cannot_see_comment(self):
-        # L'intrus tente d'accéder à la liste des commentaires
-        response = self.client_stranger.get(f'/comments/{self.comment.id}/')
-        self.assertEqual(response.status_code, 404)  # Pas autorisé / introuvable
+        url = f'/api/comments/{self.comment.id}/'
+        response = self.client_stranger.get(url)
+        print(f"RESPONSE API cannot see comment {response}")
+        self.assertEqual(response.status_code, 404)  # Non-contributeur = pas d'accès
 
     def test_contributor_can_see_comment(self):
-        response = self.client_contributor.get(f'/comments/{self.comment.id}/')
+        url = f'/api/comments/{self.comment.id}/'
+        response = self.client_contributor.get(url)
+        print(f"RESPONSE contributor can see {response}")
         self.assertEqual(response.status_code, 200)
 
     def test_author_can_update_comment(self):
+        url = f'/api/comments/{self.comment.id}/'
         data = {'description': 'Commentaire modifié'}
-        response = self.client_author.patch(f'/comments/{self.comment.id}/', data, format='json')
-
-       
+        response = self.client_author.patch(url, data, format='json')
+        print(f"RESPONSE CORRECTED author can update {response}")
         self.assertEqual(response.status_code, 200)
         self.comment.refresh_from_db()
         self.assertEqual(self.comment.description, 'Commentaire modifié')
 
     def test_contributor_cannot_update_comment(self):
+        url = f'/api/comments/{self.comment.id}/'
         data = {'description': 'Modification non autorisée'}
-        response = self.client_contributor.patch(f'/comments/{self.comment.id}/', data)
-        self.assertEqual(response.status_code, 403)  # Forbidden
+        response = self.client_contributor.patch(url, data, format='json')
+        print(f"RESPONSE 2 {response}")
+        self.assertEqual(response.status_code, 403)
